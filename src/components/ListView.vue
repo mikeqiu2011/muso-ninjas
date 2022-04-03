cff54d692b1aa1ce457fe26a951ae44223f07630
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-03 21:38:52 +0800

    additional fields of playlist and add style

--- src/components/ListView.vue
+++ src/components/ListView.vue
@@ -4,6 +4,14 @@
       <div class="thumbnail">
         <img :src="playlist.coverUrl" />
       </div>
+      <div class="info">
+        <h3>{{ playlist.title }}</h3>
+        <p>created by {{ playlist.userName }}</p>
+        <!-- <p>{{ playlist.description }}</p> -->
+      </div>
+      <div class="song-number">
+        <p>{{ playlist.songs.length }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -14,5 +22,36 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.single {
+  display: flex; /* display in single line */
+  align-items: center;
+  padding: 20px;
+  border-radius: 10px;
+  background: white;
+  margin: 16px 0;
+  transition: all ease 0.2s; /* animation effect */
+}
+.single:hover {
+  box-shadow: 1px 2px 3px rgba(50, 50, 50, 0.05);
+  transform: scale(1.02);
+  transition: all ease 0.2s;
+}
+.thumbnail {
+  max-width: 100px;
+  max-height: 100px;
+  overflow: hidden;
+  border-radius: 10px;
+}
+img {
+  max-width: 150%;
+  max-height: 150%;
+  display: block;
+}
+.info {
+  margin: 0 30px;
+}
+.song-number {
+  margin-left: auto; /* align to the right */
+}
 </style>

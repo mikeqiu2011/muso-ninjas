1a941720dce24d710656a3aea594b0464bdf56f2
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-03 11:25:26 +0800

    finetune login logic

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,5 +1,6 @@
 <template>
   <form @submit.prevent="handleSubmit">
+    <h3>Login</h3>
     <input type="email" placeholder="email" required v-model="email" />
     <input type="password" placeholder="password" required v-model="password" />
     <div class="error">{{ error }}</div>
@@ -9,18 +10,21 @@
 
 <script>
 import { ref } from "@vue/reactivity";
-import useLogin from "../../composibles/useLogin";
+import useLogin from "@/composibles/useLogin";
+import { useRouter } from "vue-router";
+
 export default {
   setup(props, context) {
     const email = ref("");
     const password = ref("");
+    const router = useRouter();
     const { error, login } = useLogin();
 
     const handleSubmit = async () => {
-      console.log(email.value, password.value);
       const res = await login(email.value, password.value);
       if (!error.value) {
-        context.emit("login");
+        // context.emit("login");
+        router.push({ name: "Home" });
       }
     };
     return { email, password, handleSubmit, error };

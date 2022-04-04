f4235bc7c5e22fdb7cdccf7e2d1cec3f61178b9b
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-04 10:52:19 +0800

    add playlist detail style

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="error">{{ error }}</div>
-  <div v-if="document">
-    {{ document.title }}
+  <div v-if="playlist" class="playlist-details">
+    <!-- playlist info -->
+    <div class="playlist-info">
+      <div class="cover">
+        <img :src="playlist.coverUrl" />
+      </div>
+      <h2>{{ playlist.title }}</h2>
+      <p class="username">created by {{ playlist.userName }}</p>
+      <p class="description">{{ playlist.description }}</p>
+    </div>
+
+    <!-- song list info -->
+    <div class="song-list">
+      <p>song list here</p>
+    </div>
   </div>
 </template>
 
@@ -11,12 +24,51 @@ export default {
   props: ["id"],
 
   setup(props) {
-    const { error, document } = getDocument("playlists", props.id);
+    // grab the return value and rename it to playlist
+    const { error, document: playlist } = getDocument("playlists", props.id);
 
-    return { error, document };
+    return { error, playlist };
   },
 };
 </script>
 
-<style>
+<style scoped>
+.playlist-details {
+  display: grid;
+  grid-template-columns: 1fr 2fr; /* divide the screen into 3 part, playlist ocupy 1, song ocupy 2 */
+  gap: 80px; /** in between */
+}
+.cover {
+  overflow: hidden;
+  border-radius: 20px;
+  position: relative;
+  padding: 160px;
+}
+.cover img {
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  min-width: 100%;
+  min-height: 100%;
+  max-width: 200%;
+  max-height: 200%;
+}
+.playlist-info {
+  text-align: center;
+}
+.playlist-info h2 {
+  text-transform: capitalize;
+  font-size: 28px;
+  margin-top: 20px;
+}
+.playlist-info p {
+  margin-bottom: 20px;
+}
+.username {
+  color: #999;
+}
+.description {
+  text-align: left;
+}
 </style>

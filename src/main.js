31f15e6cb428501291d061bc171f6d7d250a4b08
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-04 08:37:44 +0800

    prevent home page redirect to login when refresh

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { projectAuth } from './firebase/config'
 
+// global style
 import "./assets/main.css"
 
-createApp(App).use(router).mount('#app')
+
+let app
+
+projectAuth.onAuthStateChanged(() => { // when page refresh, wait until user login info is fetched
+    if (!app) { // if app already created, no need to recreate
+        app = createApp(App).use(router).mount('#app')
+    }
+
+})
+
+// createApp(App).use(router).mount('#app')
